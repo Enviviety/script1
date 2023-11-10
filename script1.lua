e55ccec14b57cebf2435118b62218d40973716ee
Enviviety <149268451+Enviviety@users.noreply.github.com>
2023-11-10 19:35:29 +0300

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -1,6 +1,8 @@
+if game.Players.LocalPlayer.Name == "nielt_62" then
 local args = {
     [1] = "a",
     [2] = "All"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+end
